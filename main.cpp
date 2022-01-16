a59bf579962b6b4b3b57fc6c632490314c170c8b
Jan Marker <jan@jangmarker.de>
2022-01-16 11:33:46 +0100

    Read params from JSON
    provide JSON file with same as default parameters (revreg)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "kestensim.h"
+#include "json.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-    Parameters p;
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " json-file" << std::endl;
+        exit(1);
+    }
+
+    std::string jsonfilepath = argv[1];
+    std::ifstream jsonfile(jsonfilepath);
+    nlohmann::json j;
+
+    jsonfile >> j;
+    Parameters p = j.get<Parameters>();
     kestensim(p);
 
     return 0;
